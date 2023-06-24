32a949f1b118d0776851e7d3d387d54aaa02c7b7
Russell <rnc720@gmail.com>
2023-06-24 12:37:44 -0500

    Foundational work layed out: - Refractored in both index and notes html to connect to to the server.js file - Added basic working logic to api and html routes

---
+++ Develop/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const fs = require('fs/promises');
+const app = express();
+const PORT = 3001;
+const apiRoutes = require('./routes/apiRoutes')
+const htmlRoutes = require('./routes/htmlRoutes')
+
+//Import data
+const db = require('./db/db.json');
+
+//Serve up public folder
+app.use(express.static('public'));
+//Parse Incoming JSON data --> JS object for us to be used in our server
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//Turns on routes
+// app.use(routes);
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes)
+
+
+
+
+
+app.listen(PORT, () => {
+	console.log(`Server listening on PORT ${PORT}!`)
+})
