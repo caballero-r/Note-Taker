ed90c647342f341f4b4dfe93c558c126cfab55e6
Russell <rnc720@gmail.com>
2023-06-25 14:29:19 -0500

    Bug fix in Get/Post request: - changed GET and POST to work with frontend

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,23 +2,36 @@ const router = require('express').Router()
 const fs = require('fs/promises')
 const db = require('../db/db.json')
 const path = require('path')
+const { v4: uuidv4 } = require('uuid')
 
 router.get('/notes', (req, res) => {
-    console.log({ method: req.method, db: db })
-	res.json(db);
+	fs.readFile(path.join(__dirname, '../db/db.json')).then(response => {
+		res.status(200).json(JSON.parse(response));
+	})
 })
 
+// Creates a new note with title and text, and adds to the database.
 router.post('/notes', (req, res) => {
-	const Task = req.body
-	db.push(Task)
-	fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(db))
-		.then(() => {
-			res.status(200).json(db)
+	const { title, text} = req.body
+
+	if (text && text) {
+		const newNote = {
+			title,
+			text,
+			id: uuidv4(),
+		}
+		fs.readFile(path.join(__dirname, '../db/db.json'), {encoding: 'utf-8'}).then(response => {
+			const parsedNotes = JSON.parse(response)
+			parsedNotes.push(newNote)
+			const dataStr = JSON.stringify(parsedNotes, null, 2)
+			fs.writeFile(path.join(__dirname, '../db/db.json'), dataStr)
+			const reply = {
+				body: newNote,
+			}
+			res.status(200).json(reply);
 		})
-		.catch((err) => {
-            console.log(err)   
-			res.status(500).json(db)
-		})
+	}
 })
+
 
 module.exports = router
